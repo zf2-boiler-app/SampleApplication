537bb0f1acebb67b8c11f0a5e1ddb0a0b6686d59
Neilime <escalle.emilien@gmail.com>
2012-11-18 18:50:18 +0100

    Add Html5 boilerplate

---
+++ config/application.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'modules' => array(
+        'Application',
+    	'AssetsBundle'
+    ),
+    'module_listener_options' => array(
+        'config_glob_paths'    => array( 'config/autoload/{,*.}{global,local}.php'),
+        'module_paths' => array('./module','./vendor')
+    )
+);
