de37f5f644507e5574b13c76c60d18e2a493783e
neilime <escalle.emilien@gmail.com>
2013-04-12 19:15:29 +0200

    Clean project

--- config/application.config.php
+++ config/application.config.php
@@ -1,16 +1,23 @@
 <?php
 return array(
     'modules' => array(
+    	//Db
+    	'BoilerAppDb',
+    	'DoctrineModule',
+    	'DoctrineORMModule',
+
     	//Display
     	'BoilerAppDisplay',
     	'AssetsBundle',
     	'TreeLayoutStack',
+    	'TwbBundle',
 
     	//Access Control
     	'BoilerAppAccessControl',
+    	'BoilerAppUser',
 
-    	//Sample Application Mdules
-    	'Application',
+    	//Sample Application Modules
+    	'Application'
     ),
     'module_listener_options' => array(
         'config_glob_paths' => array( 'config/autoload/{,*.}{global,local,private}.php'),
