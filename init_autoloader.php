2183debcf89d3aefe775783d254f3600d27036da
neilime <escalle.emilien@gmail.com>
2013-08-19 19:06:38 +0200

    Improve autoloader initialization

--- init_autoloader.php
+++ init_autoloader.php
@@ -1,4 +1,5 @@
 <?php
 //Composer autoloading
-if(file_exists('vendor/autoload.php'))$loader = include 'vendor/autoload.php';
-if(!class_exists('Zend\Loader\AutoloaderFactory'))throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+if(file_exists($sAutoloadPath = 'vendor/autoload.php'))include $sAutoloadPath;
+else throw new \LogicException('Autoload file "'.$sAutoloadPath.'" does not exist');
+if(!class_exists('Zend\Loader\AutoloaderFactory'))throw new \RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
